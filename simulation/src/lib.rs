86f86651c9a4049a998e7057e4844a97ff7f5cab
Ward Gauderis <ward.gauderis@student.uantwerpen.be>
2021-05-06 17:33:15 +0200

    configs and state

--- simulation/src/lib.rs
+++ simulation/src/lib.rs
@@ -1,23 +1,101 @@
-use std::time::Duration;
+use std::{f64::consts::PI, fmt::Error, time::Duration};
 
+use nalgebra::{DMatrix, DVector};
+use rand::{thread_rng, Rng};
+
+// possible extensions:
+// no juvenile/adult carrying capacity (= 1/n)
+// mutation_mu/sigma per trait
+// measuring intervals, histint
+// theta vector
+// phenotype is not sum -> use inner product
+// TODO don't scale by sqrt(2pi)
+
+pub struct InitConfig {
+	// initial population: columns are individuals, rows are loci
+	// population size N = population.ncols() cannot change (=6000)
+	// number of loci k = population.nrow() cannot change (=4)
+	pub population:  DMatrix<f64>,
+	// initial environment
+	// patch number n = environment.ncols() cannot change (must devide N)
+	pub environment: DVector<f64>,
+}
 
 #[derive(serde::Deserialize, Debug, Clone)]
 pub struct Config {
-    pub param1: String,
-    pub param2: String,
+	// max ticks, unlimited if None (=100000)
+	pub t_max:                Option<u64>,
+	// trait mutation probability (=0.01)
+	pub mutation_mu:          DVector<f64>,
+	// expected mutational effect size (=0.01)
+	pub mutation_sigma:       f64,
+	// bin size for mutational effects (=0.01)
+	pub mutation_step:        f64,
+	// recombinational probality (=0.01)
+	pub rec:                  f64,
+	// maximum amount of offspring (=1000)
+	pub r_max:                u64,
+	// selection strength (standard deviation)
+	pub selection_sigma:      f64,
+	// generation overlap
+	pub gamma:                f64,
+	// diploid or haploid
+	pub diploid:              bool,
+	// dispersal parameter
+	pub m:                    f64,
+	// function to determine environment and selective optima
+	pub environment_function: fn(&mut DVector<f64>, u64),
 }
 
 #[derive(Debug)]
 pub struct State {
-    population: Vec<f32>,
+	tick:        u64,
+	population:  DMatrix<f64>,
+	environment: DVector<f64>,
 }
 
-pub fn init() -> State {
-    println!("initialized simulation");
-    State { population: vec![] }
+impl State {
+	pub fn environment(&mut self, environment_function: fn(&mut DVector<f64>, u64)) {
+		environment_function(&mut self.environment, self.tick);
+	}
+
+	pub fn adult_death(&mut self, gamma: f64) -> DVector<usize> {
+		let mut rng = thread_rng();
+		(0 .. self.population.len())
+			.filter(|i| rng.gen_bool(gamma))
+			.collect()
+	}
+
+	pub fn reproduction(&mut self, r_max: u64, selection_sigma: f64) -> DVector<f64> {
+		let phenotype = self.population.row_sum();
+		self.r_max / (selection_sigma * (2.0 * PI).sqrt())
+			* (-(&self.environment - phenotype) / (2.0 * selection_sigma.pow(2))).exp()
+	}
+
+	pub fn density_regulation(&mut self, offspring: DVector<f64>) {}
+
+	pub fn dispersal(&mut self) {}
+
+	pub fn mutation(&mut self) {}
+}
+
+pub fn init(init_config: InitConfig) -> Result<State, &str> {
+	if init_config.population.ncols() % init_config.environment.ncols() != 0 {
+		return Err("Population size must be divisible by number of patches");
+	}
+
+	Ok(State {
+		tick:        0,
+		population:  init_config.population,
+		environment: init_config.environment,
+	})
 }
 
 pub fn step(state: &mut State, config: &Config) {
-    println!("step simulation");
-    std::thread::sleep(Duration::from_secs(1))
+	state.environment(config.environment_function);
+	let death = state.adult_death(config.gamma);
+	let offspring = state.reproduction(config.r_max, config.selection_sigma);
+	state.density_regulation();
+	state.dispersal();
+	state.recruitment();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::State;
-use rocket_contrib::json::Json;
-
-use simulation::{init, step};
 use std::sync::{mpsc, Arc, Mutex};
 
-use serde::{Deserialize, Serialize};
-use rocket_contrib::serve::StaticFiles;
+use rocket::State;
+use rocket_contrib::{json::Json, serve::StaticFiles};
 use rocket_cors::CorsOptions;
+use serde::{Deserialize, Serialize};
+use simulation::{init, step};
 
 #[derive(Deserialize, Clone, Debug)]
 struct Initial {
-    ticks: Option<u64>,
+	ticks: Option<u64>,
 }
 
 #[derive(Deserialize, Clone, Debug)]
@@ -21,95 +19,95 @@ struct Config;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct Data {
-    pub size: f32,
-    pub phenotype: f32,
+	pub size:      f32,
+	pub phenotype: f32,
 }
 
 struct Inner {
-    pub config: Config,
-    pub values: Vec<Data>,
-    pub killer: mpsc::Sender<()>,
+	pub config: Config,
+	pub values: Vec<Data>,
+	pub killer: mpsc::Sender<()>,
 }
 
 type Shared = Arc<Mutex<Option<Inner>>>;
 
 fn simulate(initial: Initial, shared: Shared, kill: mpsc::Receiver<()>) {
-    let mut state = init();
+	let mut state = init();
 
-    for _ in 0..initial.ticks.unwrap_or(u64::MAX) {
-        if let Err(_) = kill.try_recv() {
-            return;
-        }
+	for _ in 0 .. initial.ticks.unwrap_or(u64::MAX) {
+		if let Err(_) = kill.try_recv() {
+			return;
+		}
 
-        let mut lock = shared.lock().unwrap();
-        // step(&mut state, &inner.config);
-        println!("step");
+		let mut lock = shared.lock().unwrap();
+		// step(&mut state, &inner.config);
+		println!("step");
 
-        lock.as_mut().unwrap().values.push(Data {
-            size: 5.,
-            phenotype: 2.,
-        })
-    }
+		lock.as_mut().unwrap().values.push(Data {
+			size:      5.,
+			phenotype: 2.,
+		})
+	}
 }
 
 fn start(initial: Initial, config: Config, shared: &Shared) {
-    let (sender, receiver) = mpsc::channel();
+	let (sender, receiver) = mpsc::channel();
 
-    let inner = Inner {
-        config,
-        values: vec![],
-        killer: sender,
-    };
-    shared.lock().unwrap().get_or_insert(inner);
-    let cloned = shared.clone();
+	let inner = Inner {
+		config,
+		values: vec![],
+		killer: sender,
+	};
+	shared.lock().unwrap().get_or_insert(inner);
+	let cloned = shared.clone();
 
-    std::thread::spawn(move || simulate(initial, cloned, receiver));
+	std::thread::spawn(move || simulate(initial, cloned, receiver));
 }
 
 #[post("/start", data = "<pair>")]
 fn start_route(pair: Json<(Initial, Config)>, shared: State<Shared>) -> &'static str {
-    let result = shared
-        .lock()
-        .unwrap()
-        .as_ref()
-        .map(|_| "already running")
-        .unwrap_or_default();
+	let result = shared
+		.lock()
+		.unwrap()
+		.as_ref()
+		.map(|_| "already running")
+		.unwrap_or_default();
 
-    // double lock of mutex in one function
-    let (initial, config) = pair.into_inner();
-    start(initial, config, shared.inner());
+	// double lock of mutex in one function
+	let (initial, config) = pair.into_inner();
+	start(initial, config, shared.inner());
 
-    result
+	result
 }
 
 #[post("/stop")]
 fn stop_route(shared: State<Shared>) -> String {
-    match &mut *shared.lock().unwrap() {
-        Some(Inner { killer, .. }) => killer.send(()).unwrap(),
-        None => return "not running".to_string(),
-    }
-    String::new()
+	match &mut *shared.lock().unwrap() {
+		Some(Inner { killer, .. }) => killer.send(()).unwrap(),
+		None => return "not running".to_string(),
+	}
+	String::new()
 }
 
 #[post("/update", data = "<config>")]
 fn update_route(config: Json<Config>, shared: State<Shared>) -> String {
-    match &mut *shared.lock().unwrap() {
-        Some(inner) => inner.config = config.into_inner(),
-        None => return "not running".to_string(),
-    }
-    String::new()
+	match &mut *shared.lock().unwrap() {
+		Some(inner) => inner.config = config.into_inner(),
+		None => return "not running".to_string(),
+	}
+	String::new()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), rocket::error::Error> {
-    let routes = routes![start_route, stop_route, update_route];
-    let managed: Shared = Arc::new(Mutex::default());
+	let routes = routes![start_route, stop_route, update_route];
+	let managed: Shared = Arc::new(Mutex::default());
 
-    rocket::build()
-        .attach(CorsOptions::default().to_cors().unwrap())
-        .mount("/api", routes)
-        .mount("/", StaticFiles::from("static"))
-        .manage(managed)
-        .launch()
-        .await
+	rocket::build()
+		.attach(CorsOptions::default().to_cors().unwrap())
+		.mount("/api", routes)
+		.mount("/", StaticFiles::from("static"))
+		.manage(managed)
+		.launch()
+		.await
 }
